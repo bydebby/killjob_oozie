cdfd852f200530c56860a7bec555cf1fd098953c
Debby <101076043+bydebby@users.noreply.github.com>
2022-03-17 09:51:25 +0700

    Delete AllBundleKill.sh

--- AllBundleKill.sh
+++
@@ -1,7 +0,0 @@
-#oozie jobs -oozie http://masterbtkdi02.bpom.go.id:11000/oozie -jobtype Bundle | grep RUNN | awk -F" " '{print $1}' > listKillOozie.dat
-oozie jobs -oozie http://masterbtkdi02.bpom.go.id:11000/oozie -filter "status=RUNNING" -jobtype Bundle | grep RUNN | awk -F" " '{print $1}' > listKillOozie.dat
-for xx in `cat listKillOozie.dat`; do
-#for xx in `cat listKillOozieError.dat`; do
-        oozie job -oozie http://masterbtkdi02.bpom.go.id:11000/oozie -kill $xx
-done
-echo "Kill all bundle is done..."
